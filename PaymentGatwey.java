259d1dffdc71d01174f9038695f387d8989c34f1
DivyaRatan <41578099+DivyaRatan@users.noreply.github.com>
2018-08-04 08:57:16 -0300

    Update PaymentGatwey.java

--- PaymentGatwey.java
+++ PaymentGatwey.java
@@ -7,12 +7,6 @@ public class PaymentGatwey {
     //method to transfer the given amount from the src to target account.
     public static boolean transfer(Account sender, Account reciever, double amount) {
 
-        //If the sender account balance = the given amount then the amount is
-
-        //transferred from the sender to the reciever account and true is returned
-
-        //otherwise false is returned.
-
         boolean status;
 
         if (sender.getBalance() == amount) {
@@ -22,10 +16,14 @@ public class PaymentGatwey {
             reciever.deposit(amount);
 
             status = true;
+            //If the sender account balance = the given amount then the amount is
+
+        //transferred from the sender to the reciever account and true is returned
 
         } else {
 
             status = false;
+            //otherwise false is returned.
 
         }
 
